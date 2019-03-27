fd9e8d14e2709d766b821244d322553b61793c1a
muratsigmaz <48977862+muratsigmaz@users.noreply.github.com>
2019-03-27 10:43:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,16 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
+			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
+		} else {
+		msg.reply('Aleyküm selam, hoş geldin ^^');
+		}
+	}
+});
+
+client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
