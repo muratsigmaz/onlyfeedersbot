6f89f37e7b6d438fef688b4e491c27a6f905c7b4
muratsigmaz <48977862+muratsigmaz@users.noreply.github.com>
2019-03-27 10:45:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,12 +14,22 @@ client.on('ready', () => {
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
 		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
+			msg.author.sendMessage('Aleyküm selam, hoş geldin Dahada İleri Only Feeders'); 
 		} else {
-		msg.reply('Aleyküm selam, hoş geldin ^^');
+		msg.reply('Aleyküm selam, hoş geldin Dahada İleri Only Feeders');
 		}
 	}
 });
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
