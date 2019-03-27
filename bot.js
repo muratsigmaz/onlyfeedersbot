e9a1ec9b69aa7d35032d3214ae43080040a5cca7
muratsigmaz <48977862+muratsigmaz@users.noreply.github.com>
2019-03-27 12:13:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,56 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', async message => {
+  const ms = require('ms');
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  let u = message.mentions.users.first() || message.author;
+  if (command === "stattemizle") {
+  if (!message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")) return message.channel.send(" İstatistik ayarlanmamış.")
+  if (!message.member.hasPermission('ADMINISTRATOR'))
+  return message.channel.send(" Yetkin bulunmuyor.");
+      const a = message.guild.channels.find(channel => channel.name === "Sunucu İstatistik").delete()
+      if(!a) return console.log("guildStats")
+      const b = message.guild.channels.find(channel => channel.name === `Üye sayısı: ${message.guild.memberCount}`).delete()
+      if(!b) return console.log("guildStatsMember")
+      const c = message.guild.channels.find(channel => channel.name === `Bot sayısı: ${message.guild.members.filter(m => m.user.bot).size}`).delete()
+      if(!c) return console.log("guildStatsBot")
+      const d = message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size}`).delete() //|| message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-1}`).delete() || message.guild.channels.find(channel => channel.name === `Kanal sayısı: ${message.guild.channels.size-2}`).delete()
+      if(!d) return console.log("guildStatsChannel")
+      message.channel.send(" Kanallar temizlendi.")
+    }
+  if (command === "statayarla") {
+  if (message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")) return message.channel.send(" Zaten istatistik ayarlanmış.")
+  if (!message.member.hasPermission('ADMINISTRATOR'))
+  return message.channel.send(" Yetkin bulunmuyor.");
+  message.channel.send(`Kategori ve kanal kurulumu başlatılsın mı? başlatılacak ise **evet** yazınız.`)
+      message.channel.awaitMessages(response => response.content === 'evet', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+    .then((collected) => {
+   message.guild.createChannel('Sunucu İstatistik', 'category', [{
+  id: message.guild.id,
+  deny: ['CONNECT'],
+  deny: ['VIEW_CHANNEL']
+}]);
+
+ message.guild.createChannel(`Üye sayısı: ${message.guild.memberCount}`, 'voice')
+.then(channel =>
+ channel.setParent(message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")));
+ message.guild.createChannel(`Bot sayısı: ${message.guild.members.filter(m => m.user.bot).size}`, 'voice')
+.then(channel =>
+ channel.setParent(message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")));
+message.guild.createChannel(`Kanal sayısı: ${message.guild.channels.size}`, 'voice')
+.then(channel =>
+ channel.setParent(message.guild.channels.find(channel => channel.name === "Sunucu İstatistik")));
+  message.channel.send(" Sunucu paneli ayarlandı!")
+        })
+}
+});
+
 client.on('message', msg => {
   if (msg.content === 'sa') {
     msg.reply('**Selam Hoşgeldin.**                                                          `>`<http://instagram.com/muratsigmaz>` takip etmeyi unutma! ♥ Eggysden Hızlıyım mk');
