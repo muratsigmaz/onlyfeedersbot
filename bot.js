05355cf089799109a6411ab056dc8619d6d2408c
muratsigmaz <48977862+muratsigmaz@users.noreply.github.com>
2019-04-02 20:44:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,5 +72,33 @@ client.on('message', msg => {
   }
 });
 
+client.on('message', async message => {
+  
+  let prefix = await db.fetch(`prefix_${message.guild.id}`) || ayarlar.prefix
+  
+  let kullanıcı = message.mentions.users.first() || message.author
+  let afkdkullanıcı = await db.fetch(`afk_${message.author.id}`)
+  let afkkullanıcı = await db.fetch(`afk_${kullanıcı.id}`)
+  let sebep = afkkullanıcı
+ 
+  if (message.author.bot) return;
+  if (message.content.includes(`${prefix}afk`)) return;
+  
+  if (message.content.includes(`<@${kullanıcı.id}>`)) {
+    if (afkdkullanıcı) {
+      message.channel.send(`\`${message.author.tag}\` adlı kullanıcı artık AFK değil.`)
+      db.delete(`afk_${message.author.id}`)
+    }
+    if (afkkullanıcı) return message.channel.send(`${message.author}\`${kullanıcı.tag}\` şu anda AFK. Sebep : \`${sebep}\``)
+  }
+
+  if (!message.content.includes(`<@${kullanıcı.id}>`)) {
+    if (afkdkullanıcı) {
+      message.channel.send(`\`${message.author.tag}\` adlı kullanıcı artık AFK değil.`)
+      db.delete(`afk_${message.author.id}`)
+    }
+  }
+});
+
 
 client.login(process.env.BOT_TOKEN);
